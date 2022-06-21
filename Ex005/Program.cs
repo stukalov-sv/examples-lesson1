2a4c538a1cbf28c217cd30cfb5ab3ed6ccd99589
Sergei <energostuk@gmail.com>
2022-06-21 14:30:18 +0300

    Add Ex005

---
+++ Ex005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string userName = Console.ReadLine();
+
+if(userName.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это же МАША!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(userName);
+}
